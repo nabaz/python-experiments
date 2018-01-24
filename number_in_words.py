a489b4e1b8c54354162312693a3583872e675d09
Nabaz Maaruf <nabaz@idevelopwebsite.com>
2018-01-24 13:23:31 -0500

    integre to english words

--- number_in_words.py
+++ number_in_words.py
@@ -1,36 +1,20 @@
-class NumberInWord:
-    # TODO: finish the function
-    NUMBERS_HASH = {
-        90 : "ninety",   80 : "eighty",   70 : "seventy",
-        60 : "sixty",    50 : "fifty",    40 : "forty",
-        30 : "thirty",   20 : "twenty",   19 : "nineteen",
-        18 : "eighteen", 17 : "seventeen",16 : "sixteen",
-        15 : "fifteen",  14 : "fourteen", 13 : "thirteen",
-        12 : "twelve",   11 : "eleven",   10 : "ten",
-        9  : "nine",     8  : "eight",    7  : "seven",
-        6  : "six",      5  : "five",     4  : "four",
-        3  : "three",    2  : "two",      1  : "one",
-    }
-    def __init__(self, number):
-        self.number = number
+def number_to_words(n):
 
-    def find_100s(self):
-        str_ones = self.number % 10
-        str_hundred = self.number // 100
-        str_10th = self.number - ((str_hundred) * 100) - (str_ones)
-        if self.number // 100 / 10 > 0:
-            return "{} thousand".format(self.NUMBERS_HASH[self.number // 100 / 10])
-        else:
-            return "{} hundred {} {}".format(self.NUMBERS_HASH[str_hundred], self.NUMBERS_HASH[str_10th], self.NUMBERS_HASH[str_ones])
+    to19 = 'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen'.split()
+    tens = 'Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split()
 
-def toStr(n,base):
-   convertString = "0123456789ABCDEF"
-   if n < base:
-      return convertString[n]
-   else:
-      return toStr(n//base,base) + convertString[n%base]
+    def words(num):
+        if num < 20:
+            return to19[num-1:num]
+        if num < 100:
+            return [tens[num/10-2]] + words(num%10)
+        if num < 1000:
+            return [to19[num/100-1]] + ['Hundred'] + words(num%100)
+        for p, w in enumerate(('Thousand', 'Million', 'Billion'), 1):
+            if num < 1000**(p+1):
+                return words(num/1000**p) + [w] + words(num%1000**p)
 
-print(toStr(1000,16))
+    return ' '.join(words(n)) or "Zero"
 
-# n = NumberInWord(920)
-# print(n.find_100s())
+if __name__ == '__main__':
+    print number_to_words(111)
