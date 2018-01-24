036c95e36e26d7bc581f1fe106cab4d5dbf3c3f9
Nabaz Maaruf <nabaz@idevelopwebsite.com>
2018-01-24 13:37:20 -0500

    integer to english word - optimization

--- number_in_words.py
+++ number_in_words.py
@@ -2,7 +2,8 @@ def number_to_words(n):
 
     to19 = 'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen'.split()
     tens = 'Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split()
-
+    thousand_and_up = ['Thousand', 'Million', 'Billion']
+    
     def words(num):
         if num < 20:
             return to19[num-1:num]
@@ -10,11 +11,11 @@ def number_to_words(n):
             return [tens[num/10-2]] + words(num%10)
         if num < 1000:
             return [to19[num/100-1]] + ['Hundred'] + words(num%100)
-        for p, w in enumerate(('Thousand', 'Million', 'Billion'), 1):
+        for p, w in enumerate(thousand_and_up, 1):
             if num < 1000**(p+1):
                 return words(num/1000**p) + [w] + words(num%1000**p)
 
     return ' '.join(words(n)) or "Zero"
 
 if __name__ == '__main__':
-    print number_to_words(111)
+    print number_to_words(1129832261)
